23bef6645c1d2e20cfd509042b17c05ce58b0d58
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-06-07 22:31:45 +0900

    add main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+import src.utils as utils
 
+from src.core import load_config
+from src.core import Runner
+
+if __name__ == "__main__":
+    args = utils.get_parser().parse_args()
+    config = load_config(args.config)
+
+    config["config_path"] = args.config
+
+    runner = Runner(config)
+    runner.run()
--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,2 +1,3 @@
 from .config import load_config
 from .state import State
+from .runners import Runner
